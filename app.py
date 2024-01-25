1b449afcce932ae5293d54cf815c298b33e458cd
Hsiao Yao <tomtogo1260@gmail.com>
2024-01-26 07:59:27 +0800

    feat: obj parser

---
+++ app.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+def parse_obj(filename):
+    vertices = []
+    normals = []
+    uvs = []
+    faces = []
+    colors = []
+
+    with open(filename, 'r') as f:
+        for line in f:
+            if line.startswith('v '):
+                data = [float(x) for x in line[2:].split()]
+                vertices.append(data[:3])
+                colors.append(data[3:])
+            elif line.startswith('vn '):
+                normals.append([float(x) for x in line[3:].split()])
+            elif line.startswith('vt '):
+                uvs.append([float(x) for x in line[3:].split()])
+            elif line.startswith('f '):
+                triangle = [x.split('/') for x in line.split()[1:]]
+                triangle = [[int(x) for x in vertex] for vertex in triangle]
+                faces.append(triangle)
+
+    data = {}
+    data['vertices']    = np.array(vertices)
+    data['normals']     = np.array(normals)
+    data['uvs']         = np.array(uvs)
+    data['faces']       = np.array(faces)
+    data['colors']      = np.array(colors)
+
+    return data
+
+segmentID = '20230702185753'
+
+obj_name   = f'../full-scrolls/Scroll1.volpkg/paths/{segmentID}/{segmentID}.obj'
+label_name = 'SPOILER_20230702185753.png'
+
+parse_obj(obj_name)
+
+
