2966c62af0ed29e6d10387dcd76ab9b48d4c1d47
Hsiao Yao <tomtogo1260@gmail.com>
2024-02-23 11:49:19 +0800

    complete the getBounds function

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import numpy as np
 from utils.loader import parse_obj
-from utils.computeBoundsUtils import computeTriangleBounds
+from utils.computeBoundsUtils import getBounds, computeTriangleBounds
 
 if __name__ == "__main__":
     # obj_name = 'traingle.obj'
-    obj_name = 'quad.obj'
+    # obj_name = 'quad.obj'
+    obj_name = 'plane.obj'
 
     data = parse_obj(obj_name)
-    data = computeTriangleBounds(data)
+    box_centers, box_half_sizes = computeTriangleBounds(data)
+    getBounds(box_centers, box_half_sizes)
--- utils/computeBoundsUtils.py
+++ utils/computeBoundsUtils.py
@@ -1,9 +1,19 @@
 from utils.constants import FLOAT32_EPSILON
 import numpy as np
 
-# computes the union of the bounds of all of the given triangles and puts the resulting box in "target".
-# A bounding box is computed for the centroids of the triangles, as well, and placed in "centroidTarget".
-# These are computed together to avoid redundant accesses to bounds array.
+# computes the union of the bounds of all of the given triangles
+def getBounds(box_centers, box_half_sizes):
+    box_min = box_centers - box_half_sizes / 2
+    box_max = box_centers + box_half_sizes / 2
+
+    target_min = np.min(box_min, axis=0)
+    target_max = np.max(box_max, axis=0)
+    centroidTarget_min = np.min(box_centers, axis=0)
+    centroidTarget_max = np.max(box_centers, axis=0)
+
+    return target_min, target_max, centroidTarget_min, centroidTarget_max
+
+# precomputes the bounding box for each triangle; required for quickly calculating tree splits
 def computeTriangleBounds(data):
     vertices = data.get('vertices', np.array([]))
     faces    = data.get('faces'   , np.array([]))[:, :, 0]
@@ -11,3 +21,8 @@ def computeTriangleBounds(data):
     p = vertices[faces - 1]
     box_min = np.min(p, axis=1)
     box_max = np.max(p, axis=1)
+
+    box_centers = (box_min + box_max) / 2
+    box_half_sizes = (box_max - box_min) / 2 * (1 + FLOAT32_EPSILON)
+
+    return box_centers, box_half_sizes
