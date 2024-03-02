d3871e1c45133b582955331e17717cbbf9f04401
Hsiao Yao <tomtogo1260@gmail.com>
2024-03-03 02:48:36 +0800

    add inklabels on it

--- app.py
+++ app.py
@@ -6,13 +6,22 @@ from core.math.Triangle import Triangle
 from core.objects.MeshBVHHelper import MeshBVHHelper
 from core.utils.loader import parse_obj
 
-def drawDistaneField(bvh, center, windowSize):
-    canvas = np.zeros((windowSize, windowSize, 3), dtype=np.uint8)
+def drawDistaneField(bvh, center, windowSize, data):
+    imgSize = 500
+    canvas = np.zeros((imgSize, imgSize, 3), dtype=np.uint8)
+
+    labels = cv2.imread(os.path.join('model', '20230702185753_inklabels.png'), cv2.IMREAD_UNCHANGED)
+    h_labels, w_labels = labels.shape
 
     sampling = 50
     layer = center[2]
     gap = windowSize / sampling
+    igap = imgSize / sampling
     maxDistance = windowSize / 2
+
+    triIndices = data['faces'][:, :, 0]
+    triangles = data['vertices'][triIndices - 1]
+    triUV = data['uvs'][triIndices - 1]
 
     for i in range(sampling):
         for j in range(sampling):
@@ -21,7 +30,24 @@ def drawDistaneField(bvh, center, windowSize):
             p = np.array([x, y, layer])
             point, distance, faceIndex = bvh.closestPointToPoint(p)
 
-            canvas[j*int(gap):(j+1)*int(gap), i*int(gap):(i+1)*int(gap), :] = 255 * distance / maxDistance
+            d = np.linalg.norm(triangles[faceIndex] - np.array([0, 0, 1]), axis=1)
+            d = np.maximum(d, 0.0001)
+            weights = 1 / d
+            weights /= np.sum(weights)
+            uv_mean = np.sum(triUV[faceIndex] * weights[:, np.newaxis], axis=0)
+
+            intensity = labels[int((1 - uv_mean[1]) * h_labels), int(uv_mean[0] * w_labels)]
+
+            # normal_point = (point - center + windowSize / 2) / windowSize
+            # canvas[j*int(igap):(j+1)*int(igap), i*int(igap):(i+1)*int(igap), 0] = 255
+            # canvas[j*int(igap):(j+1)*int(igap), i*int(igap):(i+1)*int(igap), 1] = 255 * normal_point[1]
+            # canvas[j*int(igap):(j+1)*int(igap), i*int(igap):(i+1)*int(igap), 2] = 255 * normal_point[0]
+
+            canvas[j*int(igap):(j+1)*int(igap), i*int(igap):(i+1)*int(igap), 0] = intensity
+            canvas[j*int(igap):(j+1)*int(igap), i*int(igap):(i+1)*int(igap), 1] = intensity
+            canvas[j*int(igap):(j+1)*int(igap), i*int(igap):(i+1)*int(igap), 2] = intensity
+
+            # canvas[j*int(igap):(j+1)*int(igap), i*int(igap):(i+1)*int(igap), :] = 255 * distance / maxDistance
 
     cv2.imshow('Box', canvas)
     cv2.waitKey(0)
@@ -40,9 +66,7 @@ if __name__ == "__main__":
     windowSize = 2 * np.max(np.maximum(boxMin - center, boxMax - center))
     windowSize = int(1.5 * windowSize)
 
-    print(center)
-
-    drawDistaneField(bvh, center, windowSize)
+    drawDistaneField(bvh, center, windowSize, data)
 
     # depth = 0
     # helper = MeshBVHHelper(bvh)
