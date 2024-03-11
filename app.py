cd967013e1417e5efec86386756f91234d61ea90
Hsiao Yao <tomtogo1260@gmail.com>
2024-03-11 19:32:42 +0800

    find out the correct cell.tif position

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import os
+import tifffile
 from core.MeshBVH import MeshBVH
 from core.utils.cut import cut_obj, re_index
 from core.utils.loader import parse_obj, save_obj
 from utils.sdf import calculateSDF
 from utils.draw import drawImage, drawBoxes
+from utils.volume import getVolume
 
 def cut(segmentID, layer, gap):
     path = f'../full-scrolls/Scroll1.volpkg/paths/{segmentID}/{segmentID}.obj'
@@ -12,6 +14,23 @@ def cut(segmentID, layer, gap):
     data = cut_obj(data, layer, gap)
     re_index(data)
     save_obj(os.path.join('model', f'{segmentID}.obj'), data, materialName = segmentID)
+
+def calCell(boundingData):
+    boxMin = boundingData[:3]
+    boxMax = boundingData[3:]
+
+    lower = (boxMin // 500 + 1).astype('int')
+    upper = (boxMax // 500 + 1).astype('int')
+
+    for x in range(lower[0], upper[0] + 1):
+        for y in range(lower[1], upper[1] + 1):
+            for z in range(lower[2], upper[2] + 1):
+                name = 'cell_yxz_{:03d}_{:03d}_{:03d}.tif'.format(y, x, z)
+                path = os.path.join('../full-scrolls/Scroll1.volpkg/volume_grids/20230205180739', name)
+                data = tifffile.imread(path)
+                
+                # z y x (1~100 layers)
+                tifffile.imwrite(name, data[:20, :100, :50])
 
 if __name__ == "__main__":
     # cut(segmentID = '20230702185753', layer = 1000, gap = 50)
@@ -23,7 +42,9 @@ if __name__ == "__main__":
     bvh = MeshBVH(data)
 
     node = bvh._roots[0].left.left.left.left
-    points, indices, distances = calculateSDF(bvh, node)
+    # points, indices, distances = calculateSDF(bvh, node)
+
+    calCell(node.boundingData)
 
     # drawImage()
     # drawLabels(bvh, indices, node)
---
+++ utils/volume.py
@@ -0,0 +1,2 @@
+def getVolume():
+    pass
