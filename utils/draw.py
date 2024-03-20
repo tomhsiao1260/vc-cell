74e977ed83e1d4024796d83abaf0471c76f052c8
Hsiao Yao <tomtogo1260@gmail.com>
2024-03-21 06:16:26 +0800

    first try to crop the corresponding labels image in 2d

--- utils/draw.py
+++ utils/draw.py
@@ -2,6 +2,8 @@ import os
 import cv2
 import tifffile
 import numpy as np
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
 from core.objects.MeshBVHHelper import MeshBVHHelper
 
 def drawImage(path):
@@ -44,6 +46,67 @@ def drawUVs(data):
         cv2.line(image, tuple(tri[0]), tuple(tri[1]), color, 1)
         cv2.line(image, tuple(tri[1]), tuple(tri[2]), color, 1)
         cv2.line(image, tuple(tri[2]), tuple(tri[0]), color, 1)
+
+    # cv2.imshow('UV', image)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
+    silhouette_scores = []
+    for n_clusters in range(2, 11):
+        kmeans = KMeans(n_clusters=n_clusters)
+        cluster_labels = kmeans.fit_predict(data['uvs'])
+        silhouette_avg = silhouette_score(data['uvs'], cluster_labels)
+        silhouette_scores.append(silhouette_avg)
+
+    best_n_clusters = np.argmax(silhouette_scores) + 2
+    print("Best number of clusters:", best_n_clusters)
+
+    kmeans = KMeans(n_clusters=best_n_clusters)
+    cluster_labels = kmeans.fit_predict(data['uvs'])
+
+    cluster_0_data = data['uvs'][np.where(cluster_labels == 0)[0]]
+    cluster_1_data = data['uvs'][np.where(cluster_labels == 1)[0]]
+
+    box_0_min = np.min(cluster_0_data, axis=0)
+    box_0_max = np.max(cluster_0_data, axis=0)
+
+    c = (box_0_max + box_0_min) / 2
+    l = box_0_max - box_0_min
+
+    inklabelPath = 'model/SPOILER_20230702185753.png'
+    labelImage = cv2.imread(inklabelPath, cv2.IMREAD_UNCHANGED)
+    hh, ww = labelImage.shape[:2]
+
+    v = cluster_0_data
+    v[:, 0] = ww * v[:, 0]
+    v[:, 1] = hh * (1 - v[:, 1])
+    v = v.astype(int)
+
+    o_min = np.min(v, axis=0)
+    o_max = np.max(v, axis=0)
+
+    image = labelImage[o_min[0]:o_max[0], o_min[1]:o_max[1]]
+    h, w = image.shape[:2]
+
+    cv2.imshow('UV', image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    uvs = data['uvs']
+    uvs -= c
+    uvs /= l
+    uvs += 0.5
+    uvs[:, 0] = w * uvs[:, 0]
+    uvs[:, 1] = h * (1 - uvs[:, 1])
+    uvs = uvs.astype(int)
+
+    for tri in data['faces'][:,:,0] - 1:
+        if (cluster_labels[tri[0]] != 0): continue
+        u = uvs[tri]
+
+        cv2.line(image, tuple(u[0]), tuple(u[1]), color, 1)
+        cv2.line(image, tuple(u[1]), tuple(u[2]), color, 1)
+        cv2.line(image, tuple(u[2]), tuple(u[0]), color, 1)
 
     cv2.imshow('UV', image)
     cv2.waitKey(0)
